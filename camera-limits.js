d38c13cd2a2de4c8939e33844ba2282611b4ad54
neusebio11 <114317899+neusebio11@users.noreply.github.com>
2023-07-03 00:16:49 +0100

    Create camera-limits.js
    sendo sincero eu pus as minhas ideias e o raciocinio em python no chatgpt e ele gerou o codigo. bem tentei aprender a linguagem mas é muito difícil, é como aprender chinês em dias.
    
    vou fazendo assim alguns e depois alteras da maneira que mais gostas e menos chatgpt

---
+++ camera-limits.js
@@ -0,0 +1,34 @@
+AFRAME.registerComponent('camera-limits', {
+    init: function () {
+      // Define os limites de rotação da câmera
+      this.maxRotationLeftRight = 90; // em graus
+      this.maxRotationUpDown = 60; // em graus
+  
+      // Obtém o elemento da câmera
+      this.cameraEl = this.el.querySelector('a-camera');
+  
+      // Registra o evento 'componentchanged' para verificar a rotação da câmera
+      this.el.addEventListener('componentchanged', this.checkCameraRotation.bind(this));
+  
+      // Obtém o botão de teletransporte
+      this.teleportButton = document.querySelector('#teleport-button');
+  
+      // Adiciona um ouvinte de eventos ao botão de teletransporte
+      this.teleportButton.addEventListener('click', this.teleportToInitialPosition.bind(this));
+    },
+  
+    checkCameraRotation: function (event) {
+      // Verifica se o componente 'rotation' foi alterado
+      if (event.detail.name === 'rotation') {
+        // Obtém a rotação atual da câmera
+        const rotation = this.cameraEl.getAttribute('rotation');
+  
+        // Limita a rotação nos eixos X e Y
+        rotation.x = Math.max(-this.maxRotationUpDown, Math.min(this.maxRotationUpDown, rotation.x));
+        rotation.y = Math.max(-this.maxRotationLeftRight, Math.min(this.maxRotationLeftRight, rotation.y));
+  
+        // Define a nova rotação da câmera
+        this.cameraEl.setAttribute('rotation', rotation);
+      }
+    },
+  

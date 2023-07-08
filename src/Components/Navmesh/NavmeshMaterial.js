7045b718a69fae9d50a2228424e48747ccb4822e
Mário Travassos <retchut@protonmail.ch>
2023-07-08 13:16:17 +0100

    feat: implement opacity controls

---
+++ src/Components/Navmesh/NavmeshMaterial.js
@@ -0,0 +1,29 @@
+/* global AFRAME, THREE */
+
+AFRAME.registerComponent("navmesh-material", {
+	schema: {
+		opacity: {
+			default: 0.0,
+			type: "float",
+		},
+		color: {
+			default: "#6bb7cf",
+			type: "string", // because the color type doesn't do any parsing, so it's functionally equivalent to a string
+		},
+	},
+	init: function () {
+		// wait for model to load
+		this.el.addEventListener("model-loaded", () => {
+			const mesh = this.el.getObject3D("mesh");
+			// set material for all nodes in the mesh
+			mesh.traverse((node) => {
+				if (node.name !== "Scene") {
+					node.material.color.set(this.data.color);
+					node.material.transparent = true;
+					node.material.opacity = this.data.opacity;
+					console.log(node.material);
+				}
+			});
+		});
+	},
+});
--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import "./Components/Camera/CameraDebug";
 import "./Components/GUI/Button";
 // import "./Components/GUI/GUI";
 import "./Components/Navmesh/NavmeshConstraint";
+import "./Components/Navmesh/NavmeshMaterial";

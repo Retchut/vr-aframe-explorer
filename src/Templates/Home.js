d485c2f255ef1a4baf8c41d5077d9b386bd7822b
Mário Travassos <retchut@protonmail.ch>
2023-07-13 00:04:30 +0100

    feat: implement simple routes

---
+++ src/Templates/Home.js
@@ -0,0 +1,13 @@
+function HomeTemplate() {
+	const head = ``;
+
+	const html = `
+        <h1>home page</h1>
+        <p>welcome!</p>
+        <a href="/viewer">Go to viewer</a>
+    `;
+
+	return { head, html };
+}
+
+export default HomeTemplate;

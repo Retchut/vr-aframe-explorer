3d7a7661391fba6eb513a1f0a7e6c8596916057b
Mário Travassos <retchut@protonmail.ch>
2023-07-21 17:20:42 +0100

    chore: remove unused ui element

--- src/entry-client.js
+++ src/entry-client.js
@@ -4,7 +4,6 @@ import "./Components/Camera/CameraDebug";
 import "./Components/GUI/Button";
 import "./Components/Navmesh/NavmeshConstraint";
 import "./Components/Navmesh/NavmeshMaterial";
-import "./Components/GUI/MeshUI";
 import "./Components/GUI/MeshContainer";
 import "./Components/GUI/MeshBlock";
 import "./Components/GUI/MeshText";

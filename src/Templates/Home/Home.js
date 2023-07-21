e45855d60a39fc7a9e93dd7f33509be4fcc1154c
Mário Travassos <retchut@protonmail.ch>
2023-07-21 17:22:10 +0100

    chore: update home template formatting

--- src/Templates/Home/Home.js
+++ src/Templates/Home/Home.js
@@ -2,15 +2,15 @@ function HomeTemplate() {
 	const head = ``;
 
 	const html = `
-		<h1>home page</h1>
-		<p>welcome!</p>
+		<h1>Welcome</h1>
 		<h2>See the examples:</h2>
-		<a href="/example/0">Example 1</a>
-		<a href="/example/1">Example 2</a>
-		<a href="/example/2">Example 3</a>
-		<a href="/example/3">Example 4</a>
+		<a href="/example/0">Test House</a>
 		<br/>
-		<p>PS: only the 1st example exists</p>
+		<br/>
+		<a href="/example/1">Meeting Room</a>
+		<br/>
+		<br/>
+		<a href="/example/2">Hall</a>
 	`;
 
 	return { head, html };
